a0b788471e6a0a6883e0703fddb02ee420c5d70d
laomao800-Mac <laomao800@qq.com>
2019-10-04 14:47:37 +0800

    build: fix build error

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
@@ -15,6 +16,7 @@ const banner =
 const commonConfig = {
   input: 'src/index.js',
   plugins: [
+    resolve(),
     commonjs(),
     vue({
       style: {
